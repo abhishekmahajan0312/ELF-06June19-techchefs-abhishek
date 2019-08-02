896082d5e1bbc3fee3efd179d8abd656fef4eb21
Abhishek <abhishekmahajan0312@gmail.com>
2019-08-02 19:33:51 +0530

    Emp Spring Mvc Recreate

---
+++ emp-springmvc/src/main/java/com/tc/empspringmvc/beans/EmployeeOtherInfoBean.java
@@ -0,0 +1,43 @@
+package com.tc.empspringmvc.beans;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "employee_otherinfo")
+public class EmployeeOtherInfoBean {
+	@Id
+	@Column(name = "id")
+	int id;
+	@Column(name = "pan")
+	String pan;
+	@Column(name = "ismarried")
+	boolean isMarried;
+	@Column(name = "blood_grp")
+	String bloodGrp;
+	@Column(name = "ischallenged")
+	boolean isChallenged;
+	@Column(name = "emergency_contact_number")
+	long emergencyCN;
+	@Column(name = "emergency_contact_person")
+	String emergencyCP;
+	@Column(name = "nationality")
+	String nationality;
+	@Column(name = "religion")
+	String religion;
+	@Column(name = "father_nm")
+	String fatherNM;
+	@Column(name = "mother_nm")
+	String motherNM;
+	@Column(name = "spouse_nm")
+	String spouseNM;
+	@Column(name = "passport")
+	String passport;
+	@Column(name = "adhar")
+	long adhar;
+}
